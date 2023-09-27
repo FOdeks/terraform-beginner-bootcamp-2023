c877b71049aeb277828c8a28aab4f70b72e98efd
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-09-26 22:08:41 -0400

    #36 create cdn and serve s3 bucket from cdn (#37)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,10 +1,10 @@
 variable "user_uuid" {
   description = "The UUID of the User"
   type        = string
-  
+
   validation {
-    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message    = "The user_uuid value is not a valid UUID. It should be in the form of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message = "The user_uuid value is not a valid UUID. It should be in the form of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
   }
 }
 
@@ -13,8 +13,8 @@ variable "bucket_name" {
   type        = string
 
   validation {
-    condition     = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+    condition = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
       can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
