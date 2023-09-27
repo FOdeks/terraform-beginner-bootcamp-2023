9d112f685e558f19baffd3b335823bcc0e9de565
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-09-27 00:28:50 -0400

    #38 only upload index.html is a content version has changed (#39)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -40,3 +40,14 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
+
+#Content Version
+variable "content_version" {
+  description = "The content version (positive integer starting at 1)"
+  type        = number
+  
+  validation {
+    condition     = var.content_version >= 1
+    error_message = "The content_version must be a positive integer starting at 1."
+  }
+}
