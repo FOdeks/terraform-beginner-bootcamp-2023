2d727f3fd7674488310cafa12c10fe6c698faaa1
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-09-27 22:42:09 -0400

    #47 added required argument 'assets-path' to the root main.tf. (#48)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -30,7 +30,7 @@ resource "aws_s3_object" "index_html" {
   key          = "index.html"
   source       = var.index_html_filepath
   content_type = "text/html"
-  etag = filemd5(var.index_html_filepath)
+  etag         = filemd5(var.index_html_filepath)
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes       = [etag]
@@ -40,14 +40,14 @@ resource "aws_s3_object" "index_html" {
 # fileset("${path.root}/public/assets", "*.{jpg,png}")
 
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset(var.assets_path,"*.{jpg,png}")
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "assets/${each.key}"
-  source = "var.assets_path/${each.key}"
-  etag = filemd5("${var.assets_path}${each.key}")
+  for_each = fileset(var.assets_path, "*.{jpg,png}")
+  bucket   = aws_s3_bucket.website_bucket.bucket
+  key      = "assets/${each.key}"
+  source   = "var.assets_path/${each.key}"
+  etag     = filemd5("${var.assets_path}${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
-    ignore_changes = [etag]
+    ignore_changes       = [etag]
   }
 }
 
@@ -56,7 +56,7 @@ resource "aws_s3_object" "error_html" {
   key          = "error.html"
   source       = var.error_html_filepath
   content_type = "text/html"
-  etag = filemd5(var.error_html_filepath)
+  etag         = filemd5(var.error_html_filepath)
   #  lifecycle {
   #    ignore_changes = [etag]
   #  }
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -54,5 +54,5 @@ variable "content_version" {
 
 variable "assets_path" {
   description = "Path to assets folder"
-  type = string
+  type        = string
 }
