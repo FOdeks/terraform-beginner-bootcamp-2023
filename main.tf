eb82ba12cea01f8ffb2ccfe44a49af87053bc589
FOdeks <femi.odekunle@outlook.com>
2023-10-04 20:30:17 +0000

    #57 test terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 provider "terratowns" {
-  endpoint  = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token     = var.terratowns_access_token
 }
 
 # cloud {
@@ -21,23 +21,22 @@ provider "terratowns" {
 
 
 
-#module "terrahouse_aws" {
-#  source              = "./modules/terrahouse_aws"
-#  user_uuid           = var.user_uuid
-#  bucket_name         = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version     = var.content_version
-#  assets_path         = var.assets_path
-#}
+module "terrahouse_aws" {
+  source              = "./modules/terrahouse_aws"
+  user_uuid           = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version     = var.content_version
+  assets_path         = var.assets_path
+}
 
 resource "terratowns_home" "smooth_jazz" {
-  name = "Smooth Jazz Artists!"
+  name        = "Smooth Jazz Artists!"
   description = <<DESCRIPTION
 "Smooth jazz is a subgenre of contemporary jazz that emerged in the late 20th century and is characterized by its focus on creating a mellow, relaxing, and often commercially accessible sound. Smooth jazz often features a prominent use of saxophones, keyboards, and synthesizers, with a strong emphasis on melody and a generally laid-back groove. It is known for its smooth and polished production values, making it more accessible to a wider audience compared to some other jazz subgenres. Smooth jazz is often played on radio stations that specialize in easy listening or instrumental music." - ChatGPT
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "melomaniac-mansion"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fafa3.cloudfront.net"
+  town            = "missingo"
   content_version = 1
 }
