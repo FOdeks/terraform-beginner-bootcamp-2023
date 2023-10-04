bf1690ee18c9053563dc57baec903105a1efc5e0
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-10-04 13:26:40 -0400

    52 terratowns provider (#56)
    * # 52 wip setting up the skeleton for creating a custom terraform provider
    
    * #52 implement terraform provider block in terraform
    
    * #52 set up skeleton for resource
    
    * #52 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 provider "terratowns" {
-  endpoint  = "http://localhost:4567"
+  endpoint  = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -30,3 +30,14 @@ provider "terratowns" {
 #  content_version     = var.content_version
 #  assets_path         = var.assets_path
 #}
+
+resource "terratowns_home" "smooth_jazz" {
+  name = "Smooth Jazz Artists!"
+  description = <<DESCRIPTION
+"Smooth jazz is a subgenre of contemporary jazz that emerged in the late 20th century and is characterized by its focus on creating a mellow, relaxing, and often commercially accessible sound. Smooth jazz often features a prominent use of saxophones, keyboards, and synthesizers, with a strong emphasis on melody and a generally laid-back groove. It is known for its smooth and polished production values, making it more accessible to a wider audience compared to some other jazz subgenres. Smooth jazz is often played on radio stations that specialize in easy listening or instrumental music." - ChatGPT
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "melomaniac-mansion"
+  content_version = 1
+}
