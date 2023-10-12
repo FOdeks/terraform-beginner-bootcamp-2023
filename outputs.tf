270d2ad25d4235b7e6f5ae0613af07acbdae5771
FOdeks <99102643+FOdeks@users.noreply.github.com>
2023-10-11 20:40:13 -0400

    #63 update outputs.tf (#65)

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,16 @@ output "s3_website_endpoint" {
   value       = module.home_smoothjazz_hosting.website_endpoint
 }
 
+output "bucket_name" {
+  description = "Bucket name for static website hosting"
+  value       = module.home_crabdish_hosting.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "S3 static website hosting endpoint"
+  value       = module.home_crabdish_hosting.website_endpoint
+}
+
 output "cloudfront_url" {
   description = "The CloudFront Distribution Domain Name"
   value       = module.home_smoothjazz_hosting.domain_name
